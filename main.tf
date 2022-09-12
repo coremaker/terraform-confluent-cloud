22670149a19a2c560fd7af408206f0fcde946ddb
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2022-09-12 19:30:36 +0300

    Add main resources

---
+++ main.tf
@@ -0,0 +1,155 @@
+locals {
+  create_basic_cluster = var.kafka_cluster_type == "basic" ? [""] : []
+  create_standard_cluster = var.kafka_cluster_type == "standard" ? [""] : []
+  # create_basic_or_standard_cluster = var.kafka_cluster_type == "basic" || var.kafka_cluster_type == "standard" ? [""] : []
+  create_dedicated_cluster = var.kafka_cluster_type == "dedicated" ? [""] : []
+
+}
+
+# Create envinroment resource
+resource "confluent_environment" "test_environment" {
+  display_name = var.confluent_environment_name
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Create cluster resource
+resource "confluent_kafka_cluster" "test_cluster" {
+  count = var.create_cluster ? 1 : 0
+  
+  display_name = var.kafka_cluster_name
+  availability = var.kafka_cluster_availability_zone
+  cloud        = "GCP"
+  region       = var.kafka_cluster_region
+
+  dynamic "basic" {
+    for_each = local.create_basic_cluster
+    content {}
+  }
+
+  dynamic "standard" {
+    for_each = local.create_standard_cluster
+    content {}
+  }
+
+  dynamic "dedicated" {
+    for_each = local.create_dedicated_cluster
+    content {
+        cku = var.dedicated_cluster_cku
+        encryption_key = var.encryption_key
+    }
+  }
+
+  environment {
+    id = confluent_environment.test_environment.id
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Create service account
+resource "confluent_service_account" "app-manager" {
+  display_name = "app-manager-service-account"
+  description  = "Service account to manage 'test' Kafka cluster"
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.test_cluster[0].rbac_crn
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  # for_each = var.services
+  display_name = "api_key_name"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.test_cluster[0].id
+    api_version = confluent_kafka_cluster.test_cluster[0].api_version
+    kind        = confluent_kafka_cluster.test_cluster[0].kind
+
+    environment {
+      id = confluent_environment.test_environment.id
+    }
+  }
+
+    depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin
+  ]
+}
+
+# Create Topics
+resource "confluent_kafka_topic" "test_topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.test_cluster[0].id
+  }
+
+  rest_endpoint = confluent_kafka_cluster.test_cluster[0].rest_endpoint
+  for_each = var.topics
+  topic_name = each.key
+  partitions_count = each.value["partitions_count"]
+  config = each.value["config"]
+
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Create Cluster API keys with restricted access
+
+# resource "confluent_kafka_acl" "app-producer-read-on-topic" {
+#   kafka_cluster {
+#     id = confluent_kafka_cluster.test_cluster[0].id
+#   }
+#   for_each = var.services
+#   resource_type = "TOPIC"
+#   resource_name = confluent_kafka_topic.test_topic.topic_name
+#   pattern_type  = each.value["pattern_type"]
+#   principal     = "User:${confluent_service_account.app-manager.id}"
+#   host          = each.value["host"]
+#   operation     = each.value["operation"]
+#   permission    = each.value["permission"]
+#   rest_endpoint = confluent_kafka_cluster.test_cluster[0].rest_endpoint
+#   credentials {
+#     key    = confluent_api_key.app-manager-kafka-api-key.id
+#     secret = confluent_api_key.app-manager-kafka-api-key.secret
+#   }
+# }
+
+# resource "confluent_kafka_acl" "app-producer-write-on-topic" {
+#   kafka_cluster {
+#     id = confluent_kafka_cluster.test_cluster.id
+#   }
+#   for_each = var.services
+#   resource_type = "TOPIC"
+#   resource_name = confluent_kafka_topic.test_topic.topic_name
+#   pattern_type  = each.value["pattern_type"]
+#   principal     = "User:${confluent_service_account.app-manager.id}"
+#   host          = each.value["host"]
+#   operation     = each.value["operation"]
+#   permission    = each.value["permission"]
+#   rest_endpoint = confluent_kafka_cluster.test_cluster[0].rest_endpoint
+#   credentials {
+#     key    = confluent_api_key.app-manager-kafka-api-key.id
+#     secret = confluent_api_key.app-manager-kafka-api-key.secret
+#   }
+# }
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,124 @@
+# CREDENTIALS
+variable "confluent_cloud_api_key" {
+    type = string
+    description = "Confluent Cloud API Key (also referred as Cloud API ID)"
+}
+
+variable "confluent_cloud_api_secret" {
+    type = string
+    description = "Confluent Cloud API Secret"
+    sensitive = true
+}
+
+# ENVIRONMENT
+variable "confluent_environment_name" {
+    type = string
+    description = "The name of the environament you want to use"
+}
+
+variable "confluent_environment_prevent_destroy" {
+    type = bool
+    default = false
+    description = "This prevents Terraform from accidentally removing critical resources"
+}
+
+# CLUSTER
+variable create_cluster {
+    type = bool
+    default = true
+}
+
+variable "kafka_cluster_type" {
+    type = string
+    default = "basic"
+    description = ""
+}
+
+variable "kafka_cluster_name" {
+    type = string
+    description = "The name of the Kafka cluster"
+}
+
+variable "kafka_cluster_availability_zone" {
+    type = string
+    default = "SINGLE_ZONE"
+    description = "The availability zone configuration of the Kafka cluster"
+}
+
+variable "kafka_cluster_region" {
+    type = string
+    default = "us-east1"
+    description = "The configuration region of the Kafka cluster"
+}
+
+variable "kafka_cluster_prevent_destroy" {
+    type = bool
+    default = false
+    description = "This prevents Terraform from accidentally removing critical resources"
+}
+
+variable "dedicated_cluster_cku" {
+    type = number
+    default = 2
+    description = "The number of Confluent Kafka Units for Dedicated cluster types. The minimum number of CKUs for SINGLE_ZONE dedicated clusters is 1 whereas MULTI_ZONE dedicated clusters must have more than 2 CKUs"
+}
+
+variable "dedicated_cluster_encryption_key" {
+    type = string
+    default = 2
+    description = ""
+}
+
+# TOPICS
+# variable "topic_name" {
+#     type = string
+#     description = "The name of the kafka cluster topic name"
+# }
+
+# variable "partition_count" {
+#     type = number
+#     description = "The number of partitions to create in the topic, they are like physical directories"
+# }
+
+variable "kafka_topic_prevent_destroy" {
+    type = bool
+    default = false
+    description = "This prevents Terraform from accidentally removing critical resources"
+}
+
+variable "topics" {
+    type = map(object({
+        partitions_count = number
+        config = map(string)
+    }))
+    default = {}
+}
+
+# SERVICE ACCOUNT
+# variable "service_account_name" {
+#     type = string
+#     description = "The name of the Confluent service account"
+# }
+
+
+# variable "services" {
+#     type = any
+#     default = {
+#         payments = {
+#             # api_key_name = "app-manager-kafka-api-key"
+#             resource_name = ["", ""]
+#             pattern_type = "LITERAL"
+#             host = "*"
+#             operation = "READ"
+#             permission = "ALLOW"
+#         }
+#         reports = {
+#             # api_key_name = "app-manager-kafka-api-key"
+#             resource_name = ""
+#             pattern_type = "LITERAL"
+#             host = "*"
+#             operation = "WRITE"
+#             permission = "ALLOW"
+#         }
+#     }
+# }
