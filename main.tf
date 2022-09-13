f049ec523e3ab8df8bb9be610ef26e893f27d5d8
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2022-09-13 17:33:22 +0300

    Added service account resource

--- main.tf
+++ main.tf
@@ -34,45 +34,44 @@ resource "confluent_kafka_cluster" "main" {
 }
 
 # Create service account
-# resource "confluent_service_account" "app-manager" {
-#   display_name = "app-manager-service-account"
-#   description  = "Service account to manage 'test' Kafka cluster"
-# }
+resource "confluent_service_account" "app-manager" {
+  display_name = "${confluent_kafka_cluster.main.display_name}-manager"
+  description  = "Service account to manage 'main' Kafka cluster"
+}
 
-# resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
-#   principal   = "User:${confluent_service_account.app-manager.id}"
-#   role_name   = "CloudClusterAdmin"
-#   crn_pattern = confluent_kafka_cluster.test_cluster[0].rbac_crn
-# }
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.main.rbac_crn
+}
 
-# resource "confluent_api_key" "app-manager-kafka-api-key" {
-#   # for_each = var.services
-#   display_name = "api_key_name"
-#   description  = "Kafka API Key that is owned by 'app-manager' service account"
-#   owner {
-#     id          = confluent_service_account.app-manager.id
-#     api_version = confluent_service_account.app-manager.api_version
-#     kind        = confluent_service_account.app-manager.kind
-#   }
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "${confluent_kafka_cluster.main.display_name}-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
 
-#   managed_resource {
-#     id          = confluent_kafka_cluster.test_cluster[0].id
-#     api_version = confluent_kafka_cluster.test_cluster[0].api_version
-#     kind        = confluent_kafka_cluster.test_cluster[0].kind
+  managed_resource {
+    id          = confluent_kafka_cluster.main.id
+    api_version = confluent_kafka_cluster.main.api_version
+    kind        = confluent_kafka_cluster.main.kind
 
-#     environment {
-#       id = confluent_environment.test_environment.id
-#     }
-#   }
+    environment {
+      id = confluent_environment.main.id
+    }
+  }
 
-#     depends_on = [
-#     confluent_role_binding.app-manager-kafka-cluster-admin
-#   ]
-# }
+    depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin
+  ]
+}
 
 # Create Topics
 
-resource "confluent_kafka_topic" "test_topic" {
+resource "confluent_kafka_topic" "main" {
   for_each = var.topics
   topic_name = each.key
 
@@ -84,10 +83,10 @@ resource "confluent_kafka_topic" "test_topic" {
   partitions_count = each.value["partitions_count"]
   config = each.value["config"]
 
-  # credentials {
-  #   key    = confluent_api_key.app-manager-kafka-api-key.id
-  #   secret = confluent_api_key.app-manager-kafka-api-key.secret
-  # }
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
 }
 
 # Create Cluster API keys with restricted access
