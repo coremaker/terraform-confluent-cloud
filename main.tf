f53aeb633658122c1d1ea21ec1abfca35b02a8aa
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2022-09-13 17:00:19 +0300

    Modified cluster resource

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "confluent_environment" "main" {
 }
 
 # Create cluster resource
-resource "confluent_kafka_cluster" "test_cluster" {
+resource "confluent_kafka_cluster" "main" {
   count = var.create_cluster ? 1 : 0
   
   display_name = var.kafka_cluster_name
@@ -36,63 +36,63 @@ resource "confluent_kafka_cluster" "test_cluster" {
 }
 
 # Create service account
-resource "confluent_service_account" "app-manager" {
-  display_name = "app-manager-service-account"
-  description  = "Service account to manage 'test' Kafka cluster"
-}
+# resource "confluent_service_account" "app-manager" {
+#   display_name = "app-manager-service-account"
+#   description  = "Service account to manage 'test' Kafka cluster"
+# }
 
-resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
-  principal   = "User:${confluent_service_account.app-manager.id}"
-  role_name   = "CloudClusterAdmin"
-  crn_pattern = confluent_kafka_cluster.test_cluster[0].rbac_crn
-}
+# resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+#   principal   = "User:${confluent_service_account.app-manager.id}"
+#   role_name   = "CloudClusterAdmin"
+#   crn_pattern = confluent_kafka_cluster.test_cluster[0].rbac_crn
+# }
 
-resource "confluent_api_key" "app-manager-kafka-api-key" {
-  # for_each = var.services
-  display_name = "api_key_name"
-  description  = "Kafka API Key that is owned by 'app-manager' service account"
-  owner {
-    id          = confluent_service_account.app-manager.id
-    api_version = confluent_service_account.app-manager.api_version
-    kind        = confluent_service_account.app-manager.kind
-  }
+# resource "confluent_api_key" "app-manager-kafka-api-key" {
+#   # for_each = var.services
+#   display_name = "api_key_name"
+#   description  = "Kafka API Key that is owned by 'app-manager' service account"
+#   owner {
+#     id          = confluent_service_account.app-manager.id
+#     api_version = confluent_service_account.app-manager.api_version
+#     kind        = confluent_service_account.app-manager.kind
+#   }
 
-  managed_resource {
-    id          = confluent_kafka_cluster.test_cluster[0].id
-    api_version = confluent_kafka_cluster.test_cluster[0].api_version
-    kind        = confluent_kafka_cluster.test_cluster[0].kind
+#   managed_resource {
+#     id          = confluent_kafka_cluster.test_cluster[0].id
+#     api_version = confluent_kafka_cluster.test_cluster[0].api_version
+#     kind        = confluent_kafka_cluster.test_cluster[0].kind
 
-    environment {
-      id = confluent_environment.test_environment.id
-    }
-  }
+#     environment {
+#       id = confluent_environment.test_environment.id
+#     }
+#   }
 
-    depends_on = [
-    confluent_role_binding.app-manager-kafka-cluster-admin
-  ]
-}
+#     depends_on = [
+#     confluent_role_binding.app-manager-kafka-cluster-admin
+#   ]
+# }
 
-# Create Topics
-resource "confluent_kafka_topic" "test_topic" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.test_cluster[0].id
-  }
+# # Create Topics
+# resource "confluent_kafka_topic" "test_topic" {
+#   kafka_cluster {
+#     id = confluent_kafka_cluster.test_cluster[0].id
+#   }
 
-  rest_endpoint = confluent_kafka_cluster.test_cluster[0].rest_endpoint
-  for_each = var.topics
-  topic_name = each.key
-  partitions_count = each.value["partitions_count"]
-  config = each.value["config"]
+#   rest_endpoint = confluent_kafka_cluster.test_cluster[0].rest_endpoint
+#   for_each = var.topics
+#   topic_name = each.key
+#   partitions_count = each.value["partitions_count"]
+#   config = each.value["config"]
 
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
+#   credentials {
+#     key    = confluent_api_key.app-manager-kafka-api-key.id
+#     secret = confluent_api_key.app-manager-kafka-api-key.secret
+#   }
 
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+# }
 
 # Create Cluster API keys with restricted access
 
