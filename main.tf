086c0eeeb5154d9c0cc28fa86265be6d295eae8b
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2022-09-19 13:12:50 +0300

    Added app-manager service account

--- main.tf
+++ main.tf
@@ -58,60 +58,60 @@ resource "confluent_kafka_cluster" "main" {
 }
 
 # Create service account
-# resource "confluent_service_account" "app-manager" {
-#   display_name = "${confluent_kafka_cluster.main.display_name}-manager"
-#   description  = "Service account to manage 'main' Kafka cluster"
-# }
+resource "confluent_service_account" "app-manager" {
+  display_name = "${confluent_kafka_cluster.main.display_name}-manager"
+  description  = "Service account to manage 'main' Kafka cluster"
+}
 
-# resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
-#   principal   = "User:${confluent_service_account.app-manager.id}"
-#   role_name   = "CloudClusterAdmin"
-#   crn_pattern = confluent_kafka_cluster.main.rbac_crn
-# }
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.main.rbac_crn
+}
 
-# resource "confluent_api_key" "app-manager-kafka-api-key" {
-#   display_name = "${confluent_kafka_cluster.main.display_name}-key"
-#   description  = "Kafka API Key that is owned by 'app-manager' service account"
-#   owner {
-#     id          = confluent_service_account.app-manager.id
-#     api_version = confluent_service_account.app-manager.api_version
-#     kind        = confluent_service_account.app-manager.kind
-#   }
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "${confluent_kafka_cluster.main.display_name}-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
 
-#   managed_resource {
-#     id          = confluent_kafka_cluster.main.id
-#     api_version = confluent_kafka_cluster.main.api_version
-#     kind        = confluent_kafka_cluster.main.kind
+  managed_resource {
+    id          = confluent_kafka_cluster.main.id
+    api_version = confluent_kafka_cluster.main.api_version
+    kind        = confluent_kafka_cluster.main.kind
 
-#     environment {
-#       id = confluent_environment.main.id
-#     }
-#   }
+    environment {
+      id = confluent_environment.main.id
+    }
+  }
 
-#     depends_on = [
-#     confluent_role_binding.app-manager-kafka-cluster-admin
-#   ]
-# }
+    depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin
+  ]
+}
 
-# # Create Topics
+# Create Topics
 
-# resource "confluent_kafka_topic" "main" {
-#   for_each = var.topics
-#   topic_name = each.key
+resource "confluent_kafka_topic" "main" {
+  for_each = var.topics
+  topic_name = each.key
 
-#   kafka_cluster {
-#     id = confluent_kafka_cluster.main.id
-#   }
+  kafka_cluster {
+    id = confluent_kafka_cluster.main.id
+  }
 
-#   rest_endpoint = confluent_kafka_cluster.main.rest_endpoint
-#   partitions_count = each.value["partitions_count"]
-#   config = each.value["config"]
+  rest_endpoint = confluent_kafka_cluster.main.rest_endpoint
+  partitions_count = each.value["partitions_count"]
+  config = each.value["config"]
 
-#   credentials {
-#     key    = confluent_api_key.app-manager-kafka-api-key.id
-#     secret = confluent_api_key.app-manager-kafka-api-key.secret
-#   }
-# }
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
 
 # # Create Cluster API keys with restricted access
 
@@ -289,8 +289,8 @@ resource "confluent_kafka_acl" "read" {
   }
 
   credentials {
-    key    = confluent_api_key.main[each.key].id
-    secret = confluent_api_key.main[each.key].secret
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
   }
 }
 
@@ -311,7 +311,7 @@ resource "confluent_kafka_acl" "write" {
   }
 
   credentials {
-    key    = confluent_api_key.main[each.key].id
-    secret = confluent_api_key.main[each.key].secret
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
   }
 }
