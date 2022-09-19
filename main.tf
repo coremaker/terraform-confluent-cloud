a6d93109baee443fabacce7f02a1cc2ced1849dd
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2022-09-19 13:04:53 +0300

    Fixed credentials

--- main.tf
+++ main.tf
@@ -289,8 +289,8 @@ resource "confluent_kafka_acl" "read" {
   }
 
   credentials {
-    key    = confluent_api_key[each.key].id
-    secret = confluent_api_key[each.key].secret
+    key    = confluent_api_key.main[each.key].id
+    secret = confluent_api_key.main[each.key].secret
   }
 }
 
@@ -311,37 +311,7 @@ resource "confluent_kafka_acl" "write" {
   }
 
   credentials {
-    key    = confluent_api_key[each.key].id
-    secret = confluent_api_key[each.key].secret
+    key    = confluent_api_key.main[each.key].id
+    secret = confluent_api_key.main[each.key].secret
   }
 }
-
-# resource "confluent_service_account" "main" {
-#   for_each = local.services
-
-#   display_name = each.key
-#   description  = "Service account used by the  ${each.key} service on ${confluent_kafka_cluster.display_name} cluster"
-# }
-
-# resource "confluent_api_key" "app-consumer-kafka-api-key" {
-#   for_each = local.services
-
-#   display_name = "${each.key}-key"
-#   description  = "Kafka API Key that is owned by ${each.key} service account"
-
-#   owner {
-#     id          = confluent_service_account.main[each.key].id
-#     api_version = confluent_service_account.main[each.key].api_version
-#     kind        = confluent_service_account.main[each.key].kind
-#   }
-
-#   managed_resource {
-#     id          = confluent_kafka_cluster.main.id
-#     api_version = confluent_kafka_cluster.main.api_version
-#     kind        = confluent_kafka_cluster.main.kind
-
-#     environment {
-#       id = confluent_environment.main.id
-#     }
-#   }
-# }
