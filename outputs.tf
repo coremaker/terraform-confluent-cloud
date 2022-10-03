6453ed975ed4121147c3e46d743706af0e8ddac0
Cosmin Briscaru <cosmin@coremaker.io>
2022-10-03 16:49:35 +0100

    add common outputs

---
+++ outputs.tf
@@ -0,0 +1,22 @@
+output "kafka_cluster_bootstrap_endpoint" {
+  value       = confluent_kafka_cluster.main.bootstrap_endpoint
+  sensitive   = true
+  description = "The bootstrap endpoint used by Kafka clients to connect to the Kafka cluster."
+}
+
+output "kafka_cluster_rest_endpoint" {
+  value       = confluent_kafka_cluster.main.rest_endpoint
+  sensitive   = true
+  description = "The REST endpoint of the Kafka cluster."
+}
+
+output "services_keys_map" {
+  value = [
+    for key in confluent_api_key.services_api_keys : {
+      name = key.display_name
+      id = key.id
+      secret = key.secret
+    }
+  ]
+  description = "Map with service api keys pairs"
+}
